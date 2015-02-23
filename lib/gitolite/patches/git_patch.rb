1ab7867911280911787067ee4f70e35e657b6f03
Gilles DOFFE <g.doffe@gmail.com>
2015-02-23 10:33:45 +0000

    Remove development comments

--- lib/gitolite/patches/git_patch.rb
+++ lib/gitolite/patches/git_patch.rb
@@ -24,11 +24,6 @@ module GitoliteRedmine
   end
 end
 
-#ActionDispatch::Callbacks.to_prepare do 
-#  mon_fichier = File.open("/tmp/test.txt", "w")
-#  mon_fichier.write "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-#  mon_fichier.close
-#  Issue.send(:include, GitoliteRedmine::Patches::GitPatch::InstanceMethods)
 Repository::Git.safe_attributes 'branch_pattern'
 Repository::Git.safe_attributes 'tag_pattern'
 Repository::Git.send(:include, GitoliteRedmine::Patches::GitPatch) unless Repository::Git.include?(GitoliteRedmine::Patches::GitPatch)
